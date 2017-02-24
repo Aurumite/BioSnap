5d8b90cfdc2212d9084ed1cdd95bc0487425d8f3
legitdk <davkhuda@gmail.com>
2017-02-24 09:53:21 -0800

    recommitting to this app
    hi

--- BioSnapApp/app/src/main/java/com/eec136b/biosnap/biosnapapp/DataInfo.java
+++ BioSnapApp/app/src/main/java/com/eec136b/biosnap/biosnapapp/DataInfo.java
@@ -7,9 +7,6 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * Created by Davit on 2/17/2017.
  */
@@ -21,7 +18,6 @@ public class DataInfo {
     protected String unit;
     private int graphLastXValue = 0;
     protected LineGraphSeries<DataPoint> series;
-    protected Queue<DataPoint> myQueue;
 
     private final Handler mHandler = new Handler();
     private Runnable mTimer;
@@ -31,7 +27,6 @@ public class DataInfo {
         measurement = "0.00";
         unit = "unit";
         series = new LineGraphSeries<>();
-        myQueue = new LinkedList<>();
 
     }
 
@@ -40,7 +35,6 @@ public class DataInfo {
         measurement = "0.00";
         this.unit = unit;
         series = new LineGraphSeries<>();
-        myQueue = new LinkedList<>();
 
     }
 
@@ -64,7 +58,7 @@ public class DataInfo {
         mTimer = new Runnable() {
             @Override
             public void run() {
-                //series.appendData(new DataPoint(graphLastXValue++, Double.valueOf(measurement)),true,1000);
+                series.appendData(new DataPoint(graphLastXValue++, Double.valueOf(measurement)),true,1000);
                 if(graphLastXValue >= 1000){
                     DataPoint[] values = new DataPoint[1];
                     values[0] = new DataPoint(0,0);
